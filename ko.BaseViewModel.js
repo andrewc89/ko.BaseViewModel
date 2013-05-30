329b7649b8b6d8d407b764f2b580b14802faa4da
Andrew Curcie <andrew.curcie@gmail.com>
2013-05-30 16:09:50 -0400

    documentation

--- ko.BaseViewModel.js
+++ ko.BaseViewModel.js
@@ -13,20 +13,26 @@
         // object to observe form for creating new item
 	    self.newItem = {};
 
+        // list of dirty (changed) items
 	    self.dirtyItems = ko.computed(function () {
 	        return self.items().filter(function (item) {
 	            return item.dirtyFlag.isDirty();
 	        });
 	    });
 
+        // items have been changed (need to be saved)
 	    self.isDirty = ko.computed(function () {
 	        return self.dirtyItems().length > 0;
 	    });
 
+        // messages to display on page
 	    self.messages = ko.observableArray([]);
 
+	    // needs to be overridden, used to check validity of new item form
+        // and call self.save if valid
 	    self.addItem = function () { };
 
+        // saves item to server
 	    self.save = function (item, url, success) {
 	        $.ajax({
 	            type: "post",
@@ -37,8 +43,10 @@
 	        });
 	    }
 
+        // needs to be overridden, calls _saveChanges
 	    self.saveChanges = function () { };
 
+        // saves changes to list of items to server
 	    self._saveChanges = function (data, url, callback) {
 	        $.ajax({
 	            type: "post",
@@ -58,14 +66,17 @@
 	        });
 	    }
 
+        // resets dirty items
 	    function resetDirtyItems() {
 	        self.dirtyItems().map(function (item) {
 	            item.dirtyFlag.reset();
 	        });
 	    }
 
+        // needs to be overridden, used to remove item from list, call deleteItem
 	    self.removeItem = function () { };
 
+        // shows a pop-up dialog to confirm deletion of item
 	    self.confirmDeletion = function (callback) {
 	        $("#dialog").dialog({
 	            resizable: false,
@@ -79,6 +90,7 @@
 	        });
 	    }
 
+        // deletes item on server
 	    self.deleteItem = function (item, url, success) {
 	        $.ajax({
 	            type: "delete",
@@ -89,8 +101,10 @@
 	        });
 	    }
 
+        // needs to be overridden, initial load of items from server
 	    self.load = function () { };
 
+        // generic error function to use in ajax calls
 	    self.error = function (XHR, text, err) {
 	        self.messages([text + ": " + err]);
 	    };
